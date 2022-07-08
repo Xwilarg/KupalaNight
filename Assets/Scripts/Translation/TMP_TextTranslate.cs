e64d17f191f0e7d69728a2a8e573ae782a8fcde7
Xwilarg <xwilarg@protonmail.com>
2022-07-09 01:45:55 +0200

    Add translation system

---
+++ Assets/Scripts/Translation/TMP_TextTranslate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+namespace UnityWithUkraine.Translation
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TMP_TextTranslate : MonoBehaviour
+    {
+        private string _original;
+
+        public event EventHandler OnTextUpdate;
+
+        private void Start()
+        {
+            _original = GetComponent<TMP_Text>().text;
+            UpdateText();
+        }
+
+        public void UpdateText()
+        {
+            var sentence = _original;
+            foreach (var match in Regex.Matches(_original, "{([^}]+)}").Cast<Match>())
+            {
+                sentence = sentence.Replace(match.Value, Translate.Instance.Tr(match.Groups[1].Value));
+            }
+            GetComponent<TMP_Text>().text = sentence;
+            OnTextUpdate?.Invoke(this, null);
+        }
+    }
+}
