c68d27259b4eb4fceabb4ac268192da1982ecd91
Xwilarg <xwilarg@protonmail.com>
2022-07-09 14:39:48 +0200

    Add camera bounds

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -9,14 +9,21 @@ namespace UnityWithUkraine.Player
         [SerializeField]
         private Transform _target;
 
+        [SerializeField]
+        private float _min, _max;
+
+        private float _orrY;
+
         private void Awake()
         {
             _offset = _target.position - transform.position;
+            _orrY = transform.position.y;
         }
 
         private void Update()
         {
-            transform.position = _target.position - _offset;
+            var p = _target.position - _offset;
+            transform.position = new Vector3(Mathf.Clamp(p.x, _min, _max), _orrY, 0f);
         }
     }
 }
